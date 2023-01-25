d7ec7933b7266f0e59de4ae2faa959f6172e39f5
VijayGovind123 <120158003+VijayGovind123@users.noreply.github.com>
2023-01-26 03:21:47 +0530

    first commit

---
+++ passport.js
@@ -0,0 +1,19 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+passport.use(new GoogleStrategy({
+    clientID:"152936745054-4cbpovr8hd2ql58a33s40r84fubmi9ae.apps.googleusercontent.com",
+    clientSecret:"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",
+    callbackURL: "http://localhost:500/auth/callback",
+    passReqToCallback   : true
+},
+function(request, accessToken, refreshToken, profile, done) {
+        return done(null, profile);
+}
+));
