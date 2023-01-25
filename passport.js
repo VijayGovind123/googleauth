eb5bb8c9119cf64f3a45b13b2429243c9de886f2
VijayGovind123 <120158003+VijayGovind123@users.noreply.github.com>
2023-01-26 04:22:15 +0530

    Update passport.js

--- passport.js
+++ passport.js
@@ -9,7 +9,7 @@ passport.deserializeUser(function(user, done) {
 });
 passport.use(new GoogleStrategy({
     clientID:"479204704929-aifusfa1s1347gvkjk4g4afgtuhufc4u.apps.googleusercontent.com",
-    clientSecret:"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",
+    clientSecret:"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",
     callbackURL: "https://googleauth-0dcd.onrender.com/auth/callback",
     passReqToCallback   : true
 },
